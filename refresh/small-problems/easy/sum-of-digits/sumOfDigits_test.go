0d2aa630291db0a3e256b2fd85533adda10a7bdb
Torrel Moseley <trrl.mo@hotmail.com>
2024-04-05 11:42:42 -0400

    refactor sumOfDigits test

--- refresh/small-problems/easy/sum-of-digits/sumOfDigits_test.go
+++ refresh/small-problems/easy/sum-of-digits/sumOfDigits_test.go
@@ -5,28 +5,31 @@ import (
 )
 
 func TestSumOfDigits(t *testing.T) {
-	got := SumOfDigits(23)
-	want := 5
+	t.Run("test a positive number: 23", func(t *testing.T) {
+		got := SumOfDigits(23)
+		want := 5
 
-	if got != want {
-		t.Errorf("got %d want %d\n", got, want)
-	}
+		assertCorrectSum(t, got, want)
+	})
+
+	t.Run("test anoter positive number: 496", func(t *testing.T) {
+		got := SumOfDigits(496)
+		want := 19
+
+		assertCorrectSum(t, got, want)
+	})
+
+	t.Run("test yet another positive number: 123_456_789", func(t *testing.T) {
+		got := SumOfDigits(123_456_789)
+		want := 45
+
+		assertCorrectSum(t, got, want)
+	})
 }
 
-func TestSumOfDigitsAgain(t *testing.T) {
-	got := SumOfDigits(496)
-	want := 19
-
+func assertCorrectSum(t testing.TB, got, want int) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %d want %d\n", got, want)
-	}
-}
-
-func TestSumOfDigitsYetAgain(t *testing.T) {
-	got := SumOfDigits(123_456_789)
-	want := 45
-
-	if got != want {
-		t.Errorf("got %d want %d\n", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
