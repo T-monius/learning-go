6769e6cca1989bb4d36375b6d9894746faf125b5
Torrel Moseley <trrl.mo@hotmail.com>
2024-04-05 14:28:45 -0400

    add additional tests

--- refresh/small-problems/easy/stringy-strings/stringyStrings_test.go
+++ refresh/small-problems/easy/stringy-strings/stringyStrings_test.go
@@ -32,4 +32,46 @@ func TestStringyStrings(t *testing.T) {
 			t.Errorf("Output an invalid string length. Want len: %d\n", len(want))
 		}
 	})
+
+	t.Run("test num: 9", func(t *testing.T) {
+		num := 9
+		got := StringyStrings(num)
+		want := "101010101"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+		if len(want) != num {
+			t.Errorf("Output an invalid string length. Want len: %d\n", len(want))
+		}
+	})
+
+	t.Run("test num: 4", func(t *testing.T) {
+		num := 4
+		got := StringyStrings(num)
+		want := "1010"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+		if len(want) != num {
+			t.Errorf("Output an invalid string length. Want len: %d\n", len(want))
+		}
+	})
+
+	t.Run("test num: 7", func(t *testing.T) {
+		num := 7
+		got := StringyStrings(num)
+		want := "1010101"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+		if len(want) != num {
+			t.Errorf("Output an invalid string length. Want len: %d\n", len(want))
+		}
+	})
 }
